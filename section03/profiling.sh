1c9501a389a4828065e99e96775575275d07be59
Jackie Dinh <jackiedinh8@gmail.com>
2017-08-30 02:06:09 +0800

    delete unused file in section03

--- section03/profiling.sh
+++ section03/profiling.sh
@@ -1,1 +1,1 @@
-LD_PRELOAD=/home/jackie/local/lib/libprofiler.so CPUPROFILE_FREQUENCY=1000000 CPUPROFILE_REALTIME=1 CPUPROFILE=./output.prof ./echosvr_v3
+LD_PRELOAD=/home/jackie/local/lib/libprofiler.so CPUPROFILE_FREQUENCY=1000000 CPUPROFILE_REALTIME=1 CPUPROFILE=./output.prof ./echosvr_v2
